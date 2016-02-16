053cdc567dbdda1c51dce264d62dfc21ec45af49
Shane <shanodaviso@gmail.com>
2016-02-16 16:23:29 +0000

    Part a finished

---
+++ 09-sample-site/materials/Sample Site/script.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+ 
+
+ 	$("#stream1_btn").on("click", function() {
+ 		$(".stream1").removeClass('highlight_stream');
+		$(".stream2").removeClass('highlight_stream');
+		$(".stream3").removeClass('highlight_stream');
+	  	$(".stream1").addClass('highlight_stream');
+	});
+	$("#stream2_btn").on("click", function() {
+		$(".stream1").removeClass('highlight_stream');
+		$(".stream2").removeClass('highlight_stream');
+		$(".stream3").removeClass('highlight_stream');
+	  	$(".stream2").addClass('highlight_stream');
+	});
+	$("#stream3_btn").on("click", function() {
+		$(".stream1").removeClass('highlight_stream');
+		$(".stream2").removeClass('highlight_stream');
+		$(".stream3").removeClass('highlight_stream');
+	  	$(".stream3").addClass('highlight_stream');
+	});
+
+
+	$(".bottom_button").addClass('makeRed');
+
+	$(".makeRed").hover(function() {
+		$(this).removeClass("makeRed")
+	}, function() {
+		$(this).addClass('makeRed')
+	});
+
+	$(".bottom_button").hover(function() {
+		$(this).addClass("makeBorder")
+	}, function() {
+		$(this).removeClass('makeBorder')
+	});	
+
+}); 
